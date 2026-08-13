47b9fa68d62bc44c4297b04e029c979193ffe7c5
iliankt <ilian.khiter@student-cs.fr>
2026-08-13 16:29:01 +0200

    added the rng argument in the functions to calibrates on the same trajectories

--- src/branch_b/rough_bergomi.py
+++ src/branch_b/rough_bergomi.py
@@ -29,13 +29,16 @@ def build_cov(H, kappa, n):
 
     return C
 
-def build_weights(H, kappa, n):
+def build_weights(H, kappa, n, n_steps):
     alpha = H - 0.5
     return np.array([(1/n)**alpha * (k**(alpha+1) - (k-1)**(alpha+1))/(alpha+1)
-                     for k in range(kappa + 1, n)])
+                     for k in range(kappa + 1, n_steps)])
 
-def hybride_scheme_vect(H, kappa, T, n, M, L, g):
-    Z = np.random.standard_normal((M, n, kappa+1))
+def hybride_scheme_vect(H, kappa, T, n, M, L, g, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
+    n_steps = int(T * n)
+    Z = rng.standard_normal((M, n_steps, kappa+1))
     couples = Z @ L.T
 
     dW = couples[:, :, 0]
@@ -44,31 +47,36 @@ def hybride_scheme_vect(H, kappa, T, n, M, L, g):
     W = fftconvolve(dW, g[None, :], axes=1)
 
     pad = np.zeros((M, kappa+1))
-    W = np.concatenate([pad, W], axis=1)[:, :n]
+    W = np.concatenate([pad, W], axis=1)[:, :n_steps]
 
     W_hat = np.sqrt(2*H) * (I + W)
     return W_hat, dW
 
 def variance_process(W_hat, xi0, eta, H, T, n):
-    ti = np.arange(n) / n * T
+    n_steps = W_hat.shape[1]
+    ti = np.arange(n_steps) / n * T
     correction = 0.5 * eta**2 * ti**(2*H)
     V = xi0 * np.exp(eta * W_hat - correction)
     return V
 
-def price_simu_vect(S0, H, kappa, T, n, xi0, eta, rho, M, L, g):
+def price_simu_vect(S0, H, kappa, T, n, xi0, eta, rho, M, L, g, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
     dt = 1/n
-    W_hat, dW = hybride_scheme_vect(H, kappa, T, n, M, L, g)
+    W_hat, dW = hybride_scheme_vect(H, kappa, T, n, M, L, g,rng=rng)
+    n_steps = W_hat.shape[1]
     V = variance_process(W_hat, xi0, eta, H, T, n)
-    delta_W = np.random.standard_normal((M, n)) * np.sqrt(dt)
+    delta_W = rng.standard_normal((M, n_steps)) * np.sqrt(dt)
     dZ = rho*dW + np.sqrt(1-rho**2)*delta_W
     increments = -0.5 * V[:, :-1] * dt + np.sqrt(V[:, :-1]) * dZ[:, 1:]
     logS = np.concatenate([np.zeros((M, 1)), np.cumsum(increments, axis=1)], axis=1)
     return S0 * np.exp(logS)
 
 def price_forward_start(S0, H, kappa_bergomi, T1, T2, n, xi0, eta, rho, kappa_strike, M):
+    n_steps = int(T2 * n)
     i1 = int(T1/T2 * n)
     L = np.linalg.cholesky(build_cov(H, kappa_bergomi, n))
-    g = build_weights(H, kappa_bergomi, n)
+    g = build_weights(H, kappa_bergomi, n,n_steps)
     S = price_simu_vect(S0, H, kappa_bergomi, T2, n, xi0, eta, rho, M, L, g)
     payoffs = np.maximum(S[:, -1]/S[:, i1] - kappa_strike, 0)
     return payoffs.mean()
